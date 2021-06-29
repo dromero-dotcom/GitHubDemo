9edb2788c983a498e63e5b4a532096099bd336fe
dromero-dotcom <hispanouk@yahoo.co.uk>
2021-06-29 18:18:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Sample Project to upload to GitHub using Git.
+Summer 2021, by David Romero.
+This is just a simple python program: Hello World from Git and GitHub.
+Python version: 3.7.6
---
+++ helloworld_github.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 16:03:51 2021
+@author: david romero
+"""
+# Simple file to test Git and GitHub
+msg= "Hello World from Git and GitHub"
+print(msg)
